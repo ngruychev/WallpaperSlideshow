800079329b7b24664060f13e7b7c764e790cee96
Nikolay Gruychev <ngruychev@gmail.com>
2022-12-16 17:38:18 +0100

    feat: hotkey support

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,33 +29,39 @@
         private void InitializeComponent()
         {
             this.txtPath = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSelectPath = new System.Windows.Forms.Button();
             this.numSleepSeconds = new System.Windows.Forms.NumericUpDown();
             this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtNextShortcut = new System.Windows.Forms.TextBox();
+            this.btnUpdateNextShortcut = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnResetNextShortcut = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numSleepSeconds)).BeginInit();
             this.SuspendLayout();
             // 
             // txtPath
             // 
-            this.txtPath.Location = new System.Drawing.Point(12, 12);
+            this.txtPath.Location = new System.Drawing.Point(161, 12);
             this.txtPath.Name = "txtPath";
             this.txtPath.ReadOnly = true;
             this.txtPath.Size = new System.Drawing.Size(279, 23);
             this.txtPath.TabIndex = 0;
             // 
-            // button1
+            // btnSelectPath
             // 
-            this.button1.Location = new System.Drawing.Point(297, 11);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Select";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnSelectPath.Location = new System.Drawing.Point(446, 11);
+            this.btnSelectPath.Name = "btnSelectPath";
+            this.btnSelectPath.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectPath.TabIndex = 1;
+            this.btnSelectPath.Text = "Select";
+            this.btnSelectPath.UseVisualStyleBackColor = true;
+            this.btnSelectPath.Click += new System.EventHandler(this.btnSelectPath_Click);
             // 
             // numSleepSeconds
             // 
-            this.numSleepSeconds.Location = new System.Drawing.Point(12, 41);
+            this.numSleepSeconds.Location = new System.Drawing.Point(161, 41);
             this.numSleepSeconds.Maximum = new decimal(new int[] {
             3600,
             0,
@@ -76,17 +82,83 @@
             0});
             this.numSleepSeconds.ValueChanged += new System.EventHandler(this.numSleepSeconds_ValueChanged);
             // 
+            // txtNextShortcut
+            // 
+            this.txtNextShortcut.Location = new System.Drawing.Point(161, 70);
+            this.txtNextShortcut.Name = "txtNextShortcut";
+            this.txtNextShortcut.ReadOnly = true;
+            this.txtNextShortcut.Size = new System.Drawing.Size(279, 23);
+            this.txtNextShortcut.TabIndex = 3;
+            this.txtNextShortcut.TextChanged += new System.EventHandler(this.txtNextShortcut_TextChanged);
+            // 
+            // btnUpdateNextShortcut
+            // 
+            this.btnUpdateNextShortcut.Enabled = false;
+            this.btnUpdateNextShortcut.Location = new System.Drawing.Point(446, 70);
+            this.btnUpdateNextShortcut.Name = "btnUpdateNextShortcut";
+            this.btnUpdateNextShortcut.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdateNextShortcut.TabIndex = 4;
+            this.btnUpdateNextShortcut.Text = "Update";
+            this.btnUpdateNextShortcut.UseVisualStyleBackColor = true;
+            this.btnUpdateNextShortcut.Click += new System.EventHandler(this.btnUpdateNextShortcut_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Wallpaper folder:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Interval:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Next Wallpaper Shortcut:";
+            // 
+            // btnResetNextShortcut
+            // 
+            this.btnResetNextShortcut.Enabled = false;
+            this.btnResetNextShortcut.Location = new System.Drawing.Point(527, 69);
+            this.btnResetNextShortcut.Name = "btnResetNextShortcut";
+            this.btnResetNextShortcut.Size = new System.Drawing.Size(75, 23);
+            this.btnResetNextShortcut.TabIndex = 8;
+            this.btnResetNextShortcut.Text = "Reset";
+            this.btnResetNextShortcut.UseVisualStyleBackColor = true;
+            this.btnResetNextShortcut.Click += new System.EventHandler(this.btnResetNextShortcut_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 81);
+            this.ClientSize = new System.Drawing.Size(611, 110);
+            this.Controls.Add(this.btnResetNextShortcut);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdateNextShortcut);
+            this.Controls.Add(this.txtNextShortcut);
             this.Controls.Add(this.numSleepSeconds);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSelectPath);
             this.Controls.Add(this.txtPath);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Wallpaper Slideshow";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.numSleepSeconds)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -95,8 +167,14 @@
 
         #endregion
         private TextBox txtPath;
-        private Button button1;
+        private Button btnSelectPath;
         private NumericUpDown numSleepSeconds;
         private FolderBrowserDialog folderBrowserDialog;
+        private TextBox txtNextShortcut;
+        private Button btnUpdateNextShortcut;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnResetNextShortcut;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,14 @@
+using WK.Libraries.HotkeyListenerNS;
+
 namespace WallpaperSlideshow
 {
     public partial class Form1 : Form
     {
-        Slideshow _slideshow = new();
-        NotifyIcon _trayIcon = new();
+        private Slideshow _slideshow = new();
+        private NotifyIcon _trayIcon = new();
+        private HotkeyListener _hotkeyListener = new();
+        private HotkeySelector _hotkeySelector = new();
+        private Hotkey _nextHotkey = new(Keys.Control | Keys.Alt, Keys.N);
         public Form1()
         {
             InitializeComponent();
@@ -12,6 +17,7 @@ namespace WallpaperSlideshow
             // https://stackoverflow.com/a/1870823
             var iconHandle = Properties.Resources.framed_picture_icon.GetHicon();
             _trayIcon.Icon = Icon.FromHandle(iconHandle);
+            #region iconContextMenu
             _trayIcon.ContextMenuStrip = new();
             _trayIcon.ContextMenuStrip.Items.Add("Next wallpaper", null, (_, _) => nextWallpaper());
             _trayIcon.ContextMenuStrip.Items.Add("Pause", null, (_, _) => pause());
@@ -32,9 +38,29 @@ namespace WallpaperSlideshow
                 Application.Exit();
             });
             _trayIcon.ContextMenuStrip.Items[1].Enabled = false;
+            #endregion
 
             txtPath.Text = _slideshow.Path;
             numSleepSeconds.Value = _slideshow.SleepSeconds;
+
+            _nextHotkey = HotkeyListener.Convert(LocalStorage.GetItem<string>("nextHotkey") ?? "Control, Alt + N");
+            _hotkeyListener.HotkeyUpdated += (_, e) =>
+            {
+                if (e.UpdatedHotkey == _nextHotkey)
+                {
+                    LocalStorage.SetItem("nextHotkey", HotkeyListener.Convert(_nextHotkey));
+                }
+            };
+            _hotkeyListener.SuspendOn(this);
+            _hotkeyListener.Add(_nextHotkey);
+            _hotkeySelector.Enable(txtNextShortcut, _nextHotkey);
+            _hotkeyListener.HotkeyPressed += (_, e) =>
+            {
+                if (e.Hotkey == _nextHotkey)
+                {
+                    _slideshow.NextWallpaper();
+                }
+            };
 
             _slideshow.StartThread();
         }
@@ -70,7 +96,7 @@ namespace WallpaperSlideshow
             _slideshow.NextWallpaper();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnSelectPath_Click(object sender, EventArgs e)
         {
             if (folderBrowserDialog.ShowDialog() == DialogResult.OK
                 && !string.IsNullOrEmpty(folderBrowserDialog.SelectedPath))
@@ -92,6 +118,34 @@ namespace WallpaperSlideshow
         private void numSleepSeconds_ValueChanged(object sender, EventArgs e)
         {
             _slideshow.SleepSeconds = (int)numSleepSeconds.Value;
+        }
+
+        private void btnUpdateNextShortcut_Click(object sender, EventArgs e)
+        {
+            _hotkeyListener.Update(ref _nextHotkey, HotkeyListener.Convert(txtNextShortcut.Text));
+            btnUpdateNextShortcut.Enabled = false;
+            btnResetNextShortcut.Enabled = false;
+        }
+
+        private void txtNextShortcut_TextChanged(object sender, EventArgs e)
+        {
+            btnUpdateNextShortcut.Enabled = txtNextShortcut.Text != HotkeyListener.Convert(_nextHotkey);
+            btnResetNextShortcut.Enabled = btnUpdateNextShortcut.Enabled;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape && !txtNextShortcut.Focused)
+            {
+                Hide();
+            }
+        }
+
+        private void btnResetNextShortcut_Click(object sender, EventArgs e)
+        {
+            txtNextShortcut.Text = HotkeyListener.Convert(_nextHotkey);
+            btnUpdateNextShortcut.Enabled = txtNextShortcut.Text != HotkeyListener.Convert(_nextHotkey);
+            btnResetNextShortcut.Enabled = btnUpdateNextShortcut.Enabled;
         }
     }
 }
